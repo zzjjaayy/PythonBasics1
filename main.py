20253fadbc302603055fd94f29a253033ae4a19d
zzjjaayy <jayprimerathod@gmail.com>
2020-08-26 13:07:31 +0530

    Comments and input statement

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-print("Jay")
-print('o----')
-print(' ||||')
-print("jay" * 10)
-price = 10
-rate = 4.9
-is_released = True
-print(price + rate)
+# comments start with "#"
+"""this can be used as a multiline comments, but not recommended as it creates a text constant {which may cause 
+errors} """
+
+name = input("What is your name?")
+# an input statement whose return value is saved in a variable "name"
+
+print("Hi!" + name)
+# this is a string concatenation to add the variable in the string and then print it.
+
+print("Hi!", name)
+# a "comma" can also be used to use variables but only outside the inverted commas.

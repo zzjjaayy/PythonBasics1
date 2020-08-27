10a708fc6e1165fbd684722c3fff8bcaa39d7998
zzjjaayy <jayprimerathod@gmail.com>
2020-08-27 21:42:53 +0530

    While Loops

---
+++ While Loops.py
@@ -0,0 +1,14 @@
+# While Loops
+condition = True
+
+while condition:
+    print("jay")
+    condition = False
+# similar to while loops in other languages, the the lines indented{part of the block} will be
+# executed as long as the condition is true.
+
+i = 2
+while i <= 4:
+    print(i)
+    i = i + 1
+print("done")

1830c46e43f03180d531eebd2899e21d44f3ecc5
zzjjaayy <jayprimerathod@gmail.com>
2020-08-27 17:04:54 +0530

    Math Functions

---
+++ Math.py
@@ -0,0 +1,17 @@
+import math
+# imported the "math" module to use its functions
+# when imported, "math" is just like any other object, over which we can call methods
+
+# Math Functions
+
+x = 2.6
+print(round(x))
+# "round" to round off a number
+
+print(abs(-5))
+# "abs" stands for absolute, will always return a positive number{int or float}
+
+print(math.ceil(x))
+print(math.floor(x))
+# math functions like ceil and floor which return the higher and lower round off.
+# all math functions can be accessed with the "dot" operator

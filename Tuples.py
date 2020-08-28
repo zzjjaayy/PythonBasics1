f41fd9833788e28c3a450ec53dc4c65acd0caa7a
zzjjaayy <jayprimerathod@gmail.com>
2020-08-28 21:56:56 +0530

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+# Tuples
+# similar to lists, but are immutable
+# unlike lists, you can't add, remove, replace the elements.
+# you can only get information about the Tuple with methods like count(), index(), etc.
+
+numbersList = [1, 2, 3]
+# the above is a list
+
+numbersTuple = (1, 2, 3)
+numbersTuple.count(3)

58fbe040b7e7392f220148d8f3718c431bc4b436
zzjjaayy <jayprimerathod@gmail.com>
2020-08-28 11:45:54 +0530

    2D Lists

--- Lists.py
+++ Lists.py
@@ -13,3 +13,16 @@ for item in number:
     if item > largest:
         largest = item
 print(largest)
+
+# 2D Lists
+# each element in a 2D list is a list in itself.
+matrix = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9]
+]
+print(matrix[0][1])
+# the above statement will print the element at index 1 in the element at index 0
+
+for item in matrix:
+    for number in item:
+        print(number)
+# iterating all the elements of a 2D list.

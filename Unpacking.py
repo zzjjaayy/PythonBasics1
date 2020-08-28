49eb3a9418d8d8e39215b7ae9fde0f0b3bb32f44
zzjjaayy <jayprimerathod@gmail.com>
2020-08-28 22:26:19 +0530

    Unpacking

---
+++ Unpacking.py
@@ -0,0 +1,15 @@
+# Unpacking
+# shorter method to store
+# can be used with both tuples and lists
+
+coordinates = (1, 2, 3)
+
+# Method1
+x = coordinates[1]
+y = coordinates[2]
+z = coordinates[3]
+
+# Method2
+a, b, c = coordinates
+
+# Code @ line 8,9,10 does the same thing as line line 13.

a5caf0b2d8e492b47e563b232fc27205cc0df303
zzjjaayy <jayprimerathod@gmail.com>
2020-08-29 22:46:24 +0530

    Concept of Packages and Directories

---
+++ Classes/__init__.py
@@ -0,0 +1,6 @@
+# the Difference between a directory and python package is the ability to use features like
+# import statements like : import Module2
+# a normal directory {like any other folder in the computer} changes to a python package
+# with the file "__init__.py"
+# directories with "test programs and resources" can just be directories and don't need to be
+# python packages

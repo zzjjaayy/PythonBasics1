626fe8e635f334b71b1d0b9a3e322d79f93fc67e
zzjjaayy <jayprimerathod@gmail.com>
2020-08-29 10:37:17 +0530

    Basic Function

---
+++ Functions/Functions.py
@@ -0,0 +1,14 @@
+# Functions
+# a chunk of code{acts like a container} which performs a specific task.
+
+def greet_user():
+    print("Hi")
+    print("Welcome!")
+# "def" keyword for "define" for defining a function.
+
+
+print("start")
+greet_user()  # calling the function which will print two lines present in the function.
+print("finish")
+# you need to define the functions at the top and then use them.
+# if you use it
